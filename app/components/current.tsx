c692c230b2acd8402f7f2b3cf7bd43ecee1e3267
Sagar Tamang <sagar.bdr0000@gmail.com>
2025-12-28 21:15:09 +0530

    chore: add twospoon logo

--- app/components/current.tsx
+++ app/components/current.tsx
@@ -8,20 +8,21 @@ const Current = () => {
           Current
         </h1>
         <p>ai engineer at{' '}
-          <span className="inline-flex align-middle bg-white text-black rounded-sm px-0.5 py-0 hover:cursor-pointer hover:bg-white/90 active:bg-white/70 transition leading-4">
+          <span className="inline-flex align-middle gap-0.5 bg-white text-black rounded-sm px-0.5 py-0 hover:cursor-pointer hover:bg-white/90 active:bg-white/70 transition leading-4">
             <Image 
-              src={"/leapx-ai-favicon.svg"}
+              className="invert"
+              src={"/twospoon-logo-favicon.svg"}
               alt="favicon" 
-              height={10}
-              width={10} 
+              height={12}
+              width={12} 
             />
-          <a target="_blank" 
-            rel="noopener noreferrer"
-            href="https://twospoon.ai/"
-            className=""
-            >
-            twospoon.ai
-          </a>
+            <a target="_blank" 
+              rel="noopener noreferrer"
+              href="https://twospoon.ai/"
+              className=""
+              >
+              twospoon.ai
+            </a>
           </span>
           , building intelligent products that is both scalable and fast. master @ iit-patna and iit-ranchi.</p>
         <p>enjoy coding, content creation, writing, superbikes, and reading ancient greek texts.</p>
